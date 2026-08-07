0dbf0d3aa13ca93609f27aa4219a87314e207a2e
Lucas Leandro <66079160+lucasleandro1805@users.noreply.github.com>
2026-08-07 18:09:09 -0300

    Add Files/Scripts/Generators/Generator.java

---
+++ Files/Scripts/Generators/Generator.java
@@ -0,0 +1,23 @@
+package JAVARuntime;
+
+// Useful imports
+import java.util.*;
+import java.text.*;
+import java.net.*;
+import java.math.*;
+import java.io.*;
+import java.nio.*;
+
+/**
+ * @Author 
+*/
+public class Generator { 
+
+    public void start(){
+        
+    }
+
+    public int getBlockType(int x, int y, int z, int groundHeight, GenData data){
+        return -1;
+    }
+}
